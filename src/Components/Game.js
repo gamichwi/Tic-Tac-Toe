7bbea38a8ed801cf82fa750ff1ffa04dcd10df45
Gavin Williams <gamichwi@gmail.com>
2020-04-08 21:30:33 +1000

    added click handler to fill squares with either 'X' or 'O'. Added an exit if there is a winner or if the wrong square is clicked.

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -7,9 +7,16 @@ const Game = () => {
   const [xIsNext, setXisNext] = useState(true);
   const winner = calculateWinner(board);
   
-  
-  
-  const handleClick = () => {};
+  const handleClick = i => {
+    const boardCopy = [...board];
+    // If user clicks an occupied square or if the game is won, return.
+    if (winner || boardCopy[i]) return;
+    // Put an X or an O in the clicked square.
+    boardCopy[i] = xIsNext ? 'X' : 'O';
+    setBoard(boardCopy);
+    setXisNext(!xIsNext); //returns opposite value.
+
+  };
 
   const jumpTo = () => {};
 
